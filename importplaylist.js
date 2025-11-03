d6740649a36203b1997193dbc4d9dd1d783fe294
Flash98 <titankiller027@gmail.com>
2025-11-03 14:54:44 +0100

    everything works

--- importplaylist.js
+++ importplaylist.js
@@ -228,6 +228,9 @@ function pickerModel(optionsplaylist){
 
 async function startImport(file, nameplaylist) {
     const fileobject = JSON.parse(await file[0].text());
+    console.log(fileobject)
+    console.log(fileobject[0])
+    console.log(fileobject[1])
 
     const backgroundiframe = createBackgroundIframe();
 
